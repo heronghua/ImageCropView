13a5f4c592667b5de979cf0a34c1b980b03b3c6f
heronghua <heronghua1989@sina.com>
2018-04-20 15:06:50 +0800

    fix bug : 拉伸的時候 比例不正確

--- src/com/foxconn/imagecropview/CropViewI.java
+++ src/com/foxconn/imagecropview/CropViewI.java
@@ -11,11 +11,10 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 //TODO bug 待修復
 public class CropViewI extends View {
-	
+
 	private static final int NONE = 0;
 	private static final int SCALE_TOP = 1;
 	private static final int SCALE_BOTTOM = 6;
@@ -27,100 +26,95 @@ public class CropViewI extends View {
 	private static final int SCALE_LEFT_BOTTOM = 8;
 	private static final int SCALE_RIGHT_BOTTOM = 10;
 	private static final int CENTER_MOVE = 26;
-	
-	//width/height
+
+	// width/height
 	private float mRatio = 2f;
 	private static final int FINGER_RADIUS = 20;
 	private int currentState = NONE;
 	private boolean scalable;
 
-	private Rect cropShelterRect,shelterDrawableBitmapRect;
-	
-	private Bitmap shelterDrawableBitmap , backgroundBitmap , resultBitmap;
+	private Rect cropShelterRect, shelterDrawableBitmapRect;
+
+	private Bitmap shelterDrawableBitmap, backgroundBitmap, resultBitmap;
 
 	private Paint paint;
-	
-	private int resutlWidth,resultHeight;
-	
+
+	private int resutlWidth, resultHeight;
+
 	public CropViewI(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CropView);
-		shelterDrawableBitmap= ((BitmapDrawable)a.getDrawable(R.styleable.CropView_crop_shelter)).getBitmap();
-		backgroundBitmap = ((BitmapDrawable)getBackground()).getBitmap();
+		shelterDrawableBitmap = ((BitmapDrawable) a.getDrawable(R.styleable.CropView_crop_shelter)).getBitmap();
+		backgroundBitmap = ((BitmapDrawable) getBackground()).getBitmap();
 		a.recycle();
-		
-		
-		cropShelterRect = new Rect((backgroundBitmap.getWidth()-shelterDrawableBitmap.getWidth())/2,
-				(backgroundBitmap.getHeight()-shelterDrawableBitmap.getHeight())/2, 
-				(backgroundBitmap.getWidth()+shelterDrawableBitmap.getWidth())/2,
-				(backgroundBitmap.getHeight()+shelterDrawableBitmap.getHeight())/2);
-		Log.d("Crop", cropShelterRect+"");
+
+		cropShelterRect = new Rect((backgroundBitmap.getWidth() - shelterDrawableBitmap.getWidth()) / 2,
+				(backgroundBitmap.getHeight() - shelterDrawableBitmap.getHeight()) / 2,
+				(backgroundBitmap.getWidth() + shelterDrawableBitmap.getWidth()) / 2,
+				(backgroundBitmap.getHeight() + shelterDrawableBitmap.getHeight()) / 2);
+		Log.d("Crop", cropShelterRect + "");
 		paint = new Paint();
-		mRatio = shelterDrawableBitmap.getWidth()/shelterDrawableBitmap.getHeight();
-		shelterDrawableBitmapRect = new Rect(0,0,shelterDrawableBitmap.getWidth(),shelterDrawableBitmap.getHeight());
-		
-		resutlWidth=shelterDrawableBitmap.getWidth();
+		mRatio = shelterDrawableBitmap.getWidth() / shelterDrawableBitmap.getHeight();
+		shelterDrawableBitmapRect = new Rect(0, 0, shelterDrawableBitmap.getWidth(), shelterDrawableBitmap.getHeight());
+
+		resutlWidth = shelterDrawableBitmap.getWidth();
 		resultHeight = shelterDrawableBitmap.getHeight();
 	}
-	
-		
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		drawShelter(canvas);
 	}
-	
 
 	private void drawShelter(Canvas canvas) {
-		canvas.drawBitmap(shelterDrawableBitmap, shelterDrawableBitmapRect, 
-				new Rect(cropShelterRect.left,cropShelterRect.top,cropShelterRect.right,cropShelterRect.bottom),  paint);
+		canvas.drawBitmap(shelterDrawableBitmap, shelterDrawableBitmapRect,
+				new Rect(cropShelterRect.left, cropShelterRect.top, cropShelterRect.right, cropShelterRect.bottom),
+				paint);
 	}
 
 	public Bitmap getCropBitmap() {
 		
-		if (resultBitmap!=null) {
-			if(!resultBitmap.isRecycled()){
-				resultBitmap.recycle();
-			}
-		}
+		Log.e("Exception",
+				cropShelterRect.left + ":" + cropShelterRect.top + ":" + (cropShelterRect.right - cropShelterRect.left)
+						+ ":" + (cropShelterRect.bottom - cropShelterRect.top));
 		
-		resultBitmap = Bitmap.createBitmap(backgroundBitmap,cropShelterRect.left,cropShelterRect.top, 
-        		cropShelterRect.right-cropShelterRect.left,
-        		cropShelterRect.bottom-cropShelterRect.top);
+		resultBitmap = Bitmap.createBitmap(backgroundBitmap, cropShelterRect.left, cropShelterRect.top,
+				cropShelterRect.right - cropShelterRect.left, cropShelterRect.bottom - cropShelterRect.top);
 		
-		
-        return resultBitmap;
-    }  
-	
+		System.gc();
+		return resultBitmap;
+	}
+
 	@Override
 	protected void onDetachedFromWindow() {
 		super.onDetachedFromWindow();
-		
-		if (backgroundBitmap!=null) {
-			if(!backgroundBitmap.isRecycled()){
+
+		if (backgroundBitmap != null) {
+			if (!backgroundBitmap.isRecycled()) {
 				backgroundBitmap.recycle();
 			}
 		}
-		
-		if (shelterDrawableBitmap!=null) {
-			if(!shelterDrawableBitmap.isRecycled()){
+
+		if (shelterDrawableBitmap != null) {
+			if (!shelterDrawableBitmap.isRecycled()) {
 				shelterDrawableBitmap.recycle();
 			}
 		}
-		
+
 	}
-	
+
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		Bitmap bmp=((BitmapDrawable)getBackground()).getBitmap();
+		Bitmap bmp = ((BitmapDrawable) getBackground()).getBitmap();
 		setMeasuredDimension(bmp.getWidth(), bmp.getHeight());
-		
+
 	}
-	
-	private int downX,downY,preMoveX,preMoveY;
+
+	private int downX, downY, preMoveX, preMoveY;
 	private boolean actionMoved;
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 
@@ -131,7 +125,7 @@ public class CropViewI extends View {
 			actionMoved = false;
 			downX = (int) event.getX();
 			downY = (int) event.getY();
-			
+
 			this.currentState = NONE;
 			if (outerRect.contains(downX, downY)) {
 				if (Math.abs(downY - cropShelterRect.top) <= FINGER_RADIUS) {
@@ -149,180 +143,310 @@ public class CropViewI extends View {
 				if (Math.abs(downX - cropShelterRect.right) <= FINGER_RADIUS) {
 					this.currentState += SCALE_RIGHT;
 				}
-				
+
 				Rect innerRect = new Rect(cropShelterRect.left + FINGER_RADIUS, cropShelterRect.top + FINGER_RADIUS,
 						cropShelterRect.right - FINGER_RADIUS, cropShelterRect.bottom - FINGER_RADIUS);
-				if (innerRect.contains(downX, downY)){
+				if (innerRect.contains(downX, downY)) {
 					this.currentState += CENTER_MOVE;
 				}
 
 			}
-			
-//			Toast.makeText(getContext(), currentState+"", Toast.LENGTH_SHORT).show();
+
+			// Toast.makeText(getContext(), currentState+"",
+			// Toast.LENGTH_SHORT).show();
 		}
-		
+
 		if (event.getAction() == MotionEvent.ACTION_MOVE) {
 			actionMoved = true;
-			
-			//點幾點在周圍，拉伸裁剪框
-			if (scalable&&currentState != CENTER_MOVE) {
-				//TODO 
+
+			// 點幾點在周圍，拉伸裁剪框
+			if (scalable && currentState != CENTER_MOVE) {
+				// TODO
 				scale(event);
 			}
-			
-			//點幾點在中間 移動裁剪框
-			if(currentState == CENTER_MOVE){
-				if (preMoveX==0||preMoveY==0) {
-					
-					preMoveX=downX;
+
+			// 點幾點在中間 移動裁剪框
+			if (currentState == CENTER_MOVE) {
+				if (preMoveX == 0 || preMoveY == 0) {
+
+					preMoveX = downX;
 					preMoveY = downY;
 				}
-				
-				float deltX=event.getX()-preMoveX;
-				float deltY=event.getY()-preMoveY;
-				
+
+				float deltX = event.getX() - preMoveX;
+				float deltY = event.getY() - preMoveY;
+
 				cropShelterRect.left += deltX;
 				cropShelterRect.top += deltY;
 				cropShelterRect.right += deltX;
 				cropShelterRect.bottom += deltY;
-				
+
 				handleMoveOutBunds();
-				preMoveX=(int) event.getX();
-				preMoveY=(int) event.getY();
+				preMoveX = (int) event.getX();
+				preMoveY = (int) event.getY();
 			}
-			
+
 			invalidate();
-			
+
 		}
-		
-		if (MotionEvent.ACTION_UP==event.getAction()) {
-			if (!actionMoved&&Math.abs(downX-event.getX())<10) {
+
+		if (MotionEvent.ACTION_UP == event.getAction()) {
+			if (!actionMoved && Math.abs(downX - event.getX()) < 10) {
 				performClick();
 			}
-			preMoveX = 0 ;
-			preMoveY = 0 ;
+			preMoveX = 0;
+			preMoveY = 0;
 		}
 
 		return true;
 	}
 	
 	private void handleMoveOutBunds() {
-		//限制裁剪框只能在圖片裡面移動
-		if (cropShelterRect.left<0) {
-			cropShelterRect.left=0;
-			cropShelterRect.right=cropShelterRect.left + resutlWidth;
+		// 限制裁剪框只能在圖片裡面移動
+		if (cropShelterRect.left < 0) {
+			cropShelterRect.left = 0;
+			cropShelterRect.right = cropShelterRect.left + resutlWidth;
 		}
-		
-		if (cropShelterRect.right>backgroundBitmap.getWidth()) {
-			cropShelterRect.right=backgroundBitmap.getWidth();
-			cropShelterRect.left=cropShelterRect.right-resutlWidth;
-			
-		}
-		
-		
-		if (cropShelterRect.top<0) {
-			cropShelterRect.top=0;
-			cropShelterRect.bottom = cropShelterRect.top + resultHeight;
-			
-		}
-		
-		if (cropShelterRect.bottom>backgroundBitmap.getHeight()) {
-			cropShelterRect.bottom=backgroundBitmap.getHeight();
-			cropShelterRect.top = cropShelterRect.bottom - resultHeight;
-			
-		}
-	}
 
+		if (cropShelterRect.right > backgroundBitmap.getWidth()) {
+			cropShelterRect.right = backgroundBitmap.getWidth();
+			cropShelterRect.left = cropShelterRect.right - resutlWidth;
 
-	private void scale(MotionEvent event) {
-		if (event.getX()<0
-				||event.getX()>backgroundBitmap.getWidth()
-				||event.getY()>backgroundBitmap.getHeight()
-				||event.getY()<0) {
-			return;
 		}
-		
-		if (currentState==SCALE_LEFT_TOP) {
-			cropShelterRect.left = (int)event.getX();
-			resutlWidth = (int) (cropShelterRect.right-cropShelterRect.left+0.5);
-			resultHeight = (int) (resutlWidth / mRatio+0.5);
-			cropShelterRect.top = cropShelterRect.bottom - resultHeight;
-		}
-		
-		if (currentState==SCALE_RIGHT_TOP) {
-			cropShelterRect.right = (int)event.getX();
-			resutlWidth = (int) (cropShelterRect.right-cropShelterRect.left+0.5);
-			resultHeight = (int) (resutlWidth / mRatio+0.5);
-			cropShelterRect.top = cropShelterRect.bottom - resultHeight;
-		}
-		
-		if (currentState==SCALE_LEFT_BOTTOM) {
-			cropShelterRect.left = (int)event.getX();
-			resutlWidth = (int) (cropShelterRect.right-cropShelterRect.left+0.5);
-			resultHeight = (int) (resutlWidth / mRatio+0.5);
+
+		if (cropShelterRect.top < 0) {
+			cropShelterRect.top = 0;
 			cropShelterRect.bottom = cropShelterRect.top + resultHeight;
+
 		}
-		
-		if (currentState==SCALE_RIGHT_BOTTOM) {
-			cropShelterRect.right = (int)event.getX();
-			resutlWidth = (int) (cropShelterRect.right-cropShelterRect.left+0.5);
-			resultHeight = (int) (resutlWidth / mRatio+0.5);
-			cropShelterRect.bottom = cropShelterRect.top + resultHeight;
-		}
-		
-		if (currentState==SCALE_LEFT) {
-			int heigtBefore = cropShelterRect.bottom - cropShelterRect.top;
-			
-			cropShelterRect.left = (int)event.getX();
-			resutlWidth = (int) (cropShelterRect.right-cropShelterRect.left+0.5);
-			resultHeight = (int) (resutlWidth / mRatio+0.5);
-			
-			int deltY= (int) ((resultHeight-heigtBefore)/2f+0.5);
-			cropShelterRect.top = cropShelterRect.top - deltY;
-			cropShelterRect.bottom = cropShelterRect.bottom + deltY;
-		}
-		
-		if (currentState==SCALE_RIGHT) {
-			int heigtBefore = cropShelterRect.bottom - cropShelterRect.top;
-			
-			cropShelterRect.right = (int)event.getX();
-			resutlWidth = (int) (cropShelterRect.right-cropShelterRect.left+0.5);
-			resultHeight = (int) (resutlWidth / mRatio+0.5);
-			
-			int deltY= (int) ((resultHeight-heigtBefore)/2f+0.5);
-			cropShelterRect.top = cropShelterRect.top - deltY;
-			cropShelterRect.bottom = cropShelterRect.bottom + deltY;
-		}
-		
-		if (currentState==SCALE_TOP) {
-			int widthBefore = cropShelterRect.right - cropShelterRect.left;
-			
-			cropShelterRect.top = (int)event.getY();
-			resultHeight = (int) (cropShelterRect.bottom-cropShelterRect.top+0.5);
-			resutlWidth = (int) (resultHeight * mRatio+0.5);
-			
-			int deltY= (int) ((resutlWidth-widthBefore)/2f+0.5);
-			cropShelterRect.left=cropShelterRect.left-deltY;
-			cropShelterRect.right=cropShelterRect.right+deltY;
-		}
-		
-		if (currentState==SCALE_BOTTOM) {
-			int widthBefore = cropShelterRect.right - cropShelterRect.left;
-			
-			cropShelterRect.bottom = (int)event.getY();
-			resultHeight = (int) (cropShelterRect.bottom-cropShelterRect.top+0.5);
-			resutlWidth = (int) (resultHeight * mRatio+0.5);
-			
-			int deltY= (int) ((resutlWidth-widthBefore)/2f+0.5);
-			cropShelterRect.left=cropShelterRect.left-deltY;
-			cropShelterRect.right=cropShelterRect.right+deltY;
+
+		if (cropShelterRect.bottom > backgroundBitmap.getHeight()) {
+			cropShelterRect.bottom = backgroundBitmap.getHeight();
+			cropShelterRect.top = cropShelterRect.bottom - resultHeight;
+
 		}
 	}
 	
+	private void test(String state){
+		if (cropShelterRect.left<0||cropShelterRect.right>backgroundBitmap.getWidth()
+				||cropShelterRect.top<0
+				||cropShelterRect.bottom>backgroundBitmap.getHeight()) {
+			Log.d("exception", "============"+state+"===============");
+			Log.e("Exception",
+					cropShelterRect.left + ":" + cropShelterRect.top + ":" + (cropShelterRect.right - cropShelterRect.left)
+							+ ":" + (cropShelterRect.bottom - cropShelterRect.top));
+			
+			Log.d("exception", "============"+state+"===============");
+			
+		}
+		
+		
+	}
+
+	private void scale(MotionEvent event) {
+		
+		//手指移除圖片區域外，不再拉伸
+		if (event.getX() < 0 || event.getX() > backgroundBitmap.getWidth()
+				|| event.getY() > backgroundBitmap.getHeight() || event.getY() < 0) {
+			return;
+		}
+
+		if (currentState == SCALE_LEFT_TOP) {
+			
+			cropShelterRect.left = (int) event.getX();
+			
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (resutlWidth / mRatio);
+			cropShelterRect.top = cropShelterRect.bottom - resultHeight;
+			
+			test("SCALE_LEFT_TOP");
+		}
+
+		if (currentState == SCALE_RIGHT_TOP) {
+			cropShelterRect.right = (int) event.getX();
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (resutlWidth / mRatio );
+			cropShelterRect.top = cropShelterRect.bottom - resultHeight;
+			
+			test("SCALE_RIGHT_TOP");
+		}
+
+		if (currentState == SCALE_LEFT_BOTTOM) {
+			cropShelterRect.left = (int) event.getX();
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (resutlWidth / mRatio );
+			cropShelterRect.bottom = cropShelterRect.top + resultHeight;
+			
+			test("SCALE_LEFT_BOTTOM");
+		}
+
+		if (currentState == SCALE_RIGHT_BOTTOM) {
+			cropShelterRect.right = (int) event.getX();
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (resutlWidth / mRatio);
+			cropShelterRect.bottom = cropShelterRect.top + resultHeight;
+			
+			test("SCALE_RIGHT_BOTTOM");
+		}
+
+		if (currentState == SCALE_LEFT) {
+			int heigtBefore = cropShelterRect.bottom - cropShelterRect.top;
+			int predictLeft = (int) event.getX();
+			int predictWidth = (int) (cropShelterRect.right - predictLeft);
+			int predictHeight = (int) (predictWidth / mRatio);
+			
+			int predictDeltY = (int) ((predictHeight - heigtBefore) / 2f);
+			int predictTop = cropShelterRect.top - predictDeltY;
+			int predictBottom = cropShelterRect.bottom + predictDeltY;
+			if (predictTop<=0) {
+				predictTop = 0;
+				predictBottom = cropShelterRect.bottom + 2*predictDeltY;
+				
+				if (predictBottom > backgroundBitmap.getHeight()) {
+					predictBottom = backgroundBitmap.getHeight();
+					predictLeft = cropShelterRect.left;
+				}
+			}
+			
+			if (predictBottom>=backgroundBitmap.getHeight()) {
+				predictBottom = backgroundBitmap.getHeight();
+				predictTop=cropShelterRect.top - 2*predictDeltY;
+				
+				if (predictTop<=0) {
+					predictTop = 0;
+					predictLeft = cropShelterRect.left;
+				}
+			}
+
+			cropShelterRect.left = predictLeft;
+			cropShelterRect.top = predictTop;
+			cropShelterRect.bottom = predictBottom;
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (cropShelterRect.bottom - cropShelterRect.top);
+
+					
+			test("SCALE_LEFT");
+		}
+
+		if (currentState == SCALE_RIGHT) {
+			int heigtBefore = cropShelterRect.bottom - cropShelterRect.top;
+			int predictRight = (int) event.getX();
+			int predictWidth = (int) (predictRight-cropShelterRect.left);
+			int predictHeight = (int) (predictWidth / mRatio);
+			
+			int predictDeltY = (int) ((predictHeight - heigtBefore) / 2f);
+			int predictTop = cropShelterRect.top - predictDeltY;
+			int predictBottom = cropShelterRect.bottom + predictDeltY;
+			if (predictTop<=0) {
+				predictTop = 0;
+				predictBottom = cropShelterRect.bottom + 2*predictDeltY;
+				
+				if (predictBottom > backgroundBitmap.getHeight()) {
+					predictBottom = backgroundBitmap.getHeight();
+					predictRight = cropShelterRect.right;
+				}
+			}
+			
+			if (predictBottom>=backgroundBitmap.getHeight()) {
+				predictBottom = backgroundBitmap.getHeight();
+				predictTop=cropShelterRect.top - 2*predictDeltY;
+				
+				if (predictTop<=0) {
+					predictTop = 0;
+					predictRight = cropShelterRect.right;
+				}
+			}
+
+			cropShelterRect.right = predictRight;
+			cropShelterRect.top = predictTop;
+			cropShelterRect.bottom = predictBottom;
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (cropShelterRect.bottom - cropShelterRect.top);
+			
+			test("SCALE_RIGHT");
+		}
+
+		if (currentState == SCALE_TOP) {
+			int widthBefore = cropShelterRect.right - cropShelterRect.left;
+			int predictTop = (int) event.getY();
+			int predictHeight = (int) (cropShelterRect.bottom - predictTop);
+			int predictWidth  = (int) (predictHeight * mRatio);
+			
+			int predictDeltX = (int) ((predictWidth - widthBefore) / 2f);
+			int predictLeft = cropShelterRect.left - predictDeltX;
+			int predictRight = cropShelterRect.right + predictDeltX;
+			if (predictLeft<=0) {
+				predictLeft = 0;
+				predictRight = cropShelterRect.right + 2*predictDeltX;
+				
+				if (predictRight > backgroundBitmap.getWidth()) {
+					predictRight = backgroundBitmap.getWidth();
+					predictTop = cropShelterRect.top;
+				}
+			}
+			
+			if (predictRight>=backgroundBitmap.getWidth()) {
+				predictRight = backgroundBitmap.getWidth();
+				predictLeft=cropShelterRect.left - 2*predictDeltX;
+				
+				if (predictLeft<=0) {
+					predictLeft = 0;
+					predictTop = cropShelterRect.top;
+				}
+			}
+
+			cropShelterRect.right = predictRight;
+			cropShelterRect.top = predictTop;
+			cropShelterRect.left = predictLeft;
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (cropShelterRect.bottom - cropShelterRect.top);
+			
+			test("SCALE_TOP");
+		}
+
+		if (currentState == SCALE_BOTTOM) {
+			int widthBefore = cropShelterRect.right - cropShelterRect.left;
+			int predictBottom = (int) event.getY();
+			int predictHeight = (int) (predictBottom - cropShelterRect.top);
+			int predictWidth  = (int) (predictHeight * mRatio);
+			
+			int predictDeltX = (int) ((predictWidth - widthBefore) / 2f);
+			int predictLeft = cropShelterRect.left - predictDeltX;
+			int predictRight = cropShelterRect.right + predictDeltX;
+			if (predictLeft<=0) {
+				predictLeft = 0;
+				predictRight = cropShelterRect.right + 2*predictDeltX;
+				
+				if (predictRight > backgroundBitmap.getWidth()) {
+					predictRight = backgroundBitmap.getWidth();
+					predictBottom = cropShelterRect.bottom;
+				}
+			}
+			
+			if (predictRight>=backgroundBitmap.getWidth()) {
+				predictRight = backgroundBitmap.getWidth();
+				predictLeft=cropShelterRect.left - 2*predictDeltX;
+				
+				if (predictLeft<=0) {
+					predictLeft = 0;
+					predictBottom = cropShelterRect.bottom;
+				}
+			}
+
+			cropShelterRect.right = predictRight;
+			cropShelterRect.bottom = predictBottom;
+			cropShelterRect.left = predictLeft;
+			resutlWidth = (int) (cropShelterRect.right - cropShelterRect.left);
+			resultHeight = (int) (cropShelterRect.bottom - cropShelterRect.top);
+			
+			test("SCALE_BOTTOM");
+		}
+	}
+
 	public void setScalable(boolean scalable) {
 		this.scalable = scalable;
 	}
-	
+
 	public void setRatio(float mRatio) {
 		this.mRatio = mRatio;
 		invalidate();
